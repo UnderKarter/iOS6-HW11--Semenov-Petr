3242b4fed3ff178cba68a8194099a5a7b317c4e9
Semenov Petr <under_karter49@icloud.com>
2022-05-20 16:50:49 +0300

    Initial Commit

---
+++ iOS6-HW11- Semenov Petr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS6-HW11- Semenov Petr
+//
+//  Created by temp user on 20.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
