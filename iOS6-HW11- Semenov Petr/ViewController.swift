71f4e3723e1e2ee5aa86d879d67ab62ac4ec432e
Semenov Petr <under_karter49@icloud.com>
2022-05-21 15:07:43 +0300

    Made a circle avatar view.

--- iOS6-HW11- Semenov Petr/ViewController.swift
+++ iOS6-HW11- Semenov Petr/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userAvatar: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        userAvatar.layer.cornerRadius = userAvatar.bounds.height / 2
     }
 
 
